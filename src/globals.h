ff3f12b6cba277a40548a0e000d39bb147094d30
Omar Ben Brahim <om3rbb@gmail.com>
2024-12-30 17:14:08 +0100

    define and declare global variables

---
+++ src/globals.h
@@ -0,0 +1,21 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+// Screen dimensions
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+// Globals struct
+typedef struct {
+    SDL_Window *g_window;
+    SDL_Renderer *g_renderer;
+    TTF_Font *g_font;
+} Globals;
+
+// extern declaration of the global variables
+extern Globals globals;
+
+#endif
