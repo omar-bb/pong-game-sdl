47ff053034fcfa0acf6d0efdb9fe18aa435df0f2
Omar Ben Brahim <om3rbb@gmail.com>
2025-01-01 02:01:48 +0100

    adding some macros

--- src/globals.h
+++ src/globals.h
@@ -11,6 +11,14 @@
 // Borders dimensions
 #define BORDER_H 15
 
+#define PI 3.14159265359f
+#define TAU (2.0f * PI)
+#define PI_2 (PI / 2.0f)
+#define PI_4 (PI / 4.0f)
+
+#define DEG2RAD(_d) ((_d) * (PI / 180.0f))
+#define RAD2DEG(_d) ((_d) * (180.0f / PI))
+
 // Globals struct
 typedef struct {
     SDL_Window *g_window;
